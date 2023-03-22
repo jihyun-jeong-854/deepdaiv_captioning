4d6b51336a318f350cac055a16f38cd7fd47cb7d
rachel618 <jungjihyun8762@gmail.com>
2023-03-22 12:57:02 +0900

    Update cap_cls_dataset.py
    get only image data from img directory

--- cap_cls_dataset.py
+++ cap_cls_dataset.py
@@ -8,7 +8,8 @@ import torchvision.transforms as transforms
 class ImageFolderDataset(Dataset):
     def __init__(self, folder_path, transform=None):
         self.folder_path = folder_path
-        self.image_names = os.listdir(folder_path)
+        self.image_names = [file for file in os.listdir(folder_path) if not file.endswith('txt')]
+#         self.image_names = os.listdir(folder_path)
         self.transform = transform
     
     def __getitem__(self, idx):
@@ -27,7 +28,9 @@ class ImageFolderDataset(Dataset):
 # Gradio 형식에 맞게 수정
 class ImageListDatasetFromGradio(Dataset):
     def __init__(self, image_list, transform=None):
+        image_list = [file for file in image_list if not file.endswith('txt')] # 결과 파일 제거
         self.image_list = image_list
+        
         self.transform = transform
 
     def __getitem__(self, index):
